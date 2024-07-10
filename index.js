209b97addb875c41b78fa913ca501f157cfc604f
CalebCov <calebinfour@gmail.com>
2024-07-10 18:32:21 -0500

    Update js functionality

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const displayInput = document.getElementById('inputValue')
 
+//Variables
+const operators = ['-', '+', '%', '*', '/']
+let operations = []
+let currValue = ''
+
 //Functions & Operations
 
-
 function handleInteraction(value) {
+    console.log(value)
+    if (operators.includes(value)) {
+        console.log('Clicked an operator: ', value)
+        handleOperatorInput(value)
+    } else {
+        console.log('Clicked a numeric value: ', value)
+        handleNumericInput(value)
+    }
+    updateUI()
+}
+
+function handleNumericInput(value) {
+    currValue += value
+   
+}
+
+function handleOperatorInput(value) {
+    if (!currValue) {
+        return
+    }
+
+    operations.push(currValue)
+    operations.push(value)
+    currValue = ''
 
 }
 
-function handleNumericInput() {
-    
-}
+function handleEvaluate() {
 
-function handleOperatorInput() {
-    
 }
 
 function handleReset() {
-    
+    currValue = ''
+    operations = []
+    updateUI()
 }
 
 function updateUI() {
-    
-}
+    const displayString = operations.join(' ') +  ' ' + currValue
+    displayInput.innerText = displayString.trim() ? displayString : '0'
+} 
