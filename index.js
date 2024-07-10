9ffd4cf501ba5c89d207811bd95ca532de5a5391
CalebCov <calebinfour@gmail.com>
2024-07-10 18:05:23 -0500

    Start adding functions for buttons

---
+++ index.js
@@ -0,0 +1,24 @@
+const displayInput = document.getElementById('inputValue')
+
+//Functions & Operations
+
+
+function handleInteraction(value) {
+
+}
+
+function handleNumericInput() {
+    
+}
+
+function handleOperatorInput() {
+    
+}
+
+function handleReset() {
+    
+}
+
+function updateUI() {
+    
+}
